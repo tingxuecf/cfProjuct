cfa1a2425bdcf378e4f7a4647efbd8f5770b8871
tingxuecf <1638437666@qq.com>
2016-10-20 17:49:27 +0800

    修改了MainActivity中的UI控件

--- app/src/main/java/com/phone/administrator/cfprojuct/MainActivity.java
+++ app/src/main/java/com/phone/administrator/cfprojuct/MainActivity.java
@@ -2,8 +2,12 @@ package com.phone.administrator.cfprojuct;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //创建UI控件
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
